a63d9bac3f4ca6196ba3f86c172485d1533afa10
Huy Tran <huy.tranduc@axonactive.vn>
2014-07-29 15:04:39 +0700

    Add more functon to adb.py

--- adb.py
+++ adb.py
@@ -16,3 +16,58 @@ class ADB(object):
         result = self.call("devices")
         devices = result.partition('\n')[2].replace('\n', '').split('\tdevice')
         return [device for device in devices if len(device) > 2]
+
+    def upload(self, fr, to):
+        result = self.call("push " + fr + " " + to)
+        return result
+    
+    def get(self, fr, to):
+        result = self.call("pull " + fr + " " + to)
+        return result
+
+    def install(self, apk, param):
+        result = self.call("install " + param + " " + apk)
+        return result
+
+    def uninstall(self, package):
+        result = self.call("shell pm uninstall " + package)
+        return result
+
+    def clearData(self, package):
+        result = self.call("shell pm clear " + package)
+        return result
+
+    def shell(self):
+        result = self.call("shell")
+        return result
+
+    def kill(self, package):
+        result = self.call("kill " + package)
+        return result
+
+    def start(self, app):
+        pack = app.split()
+        result = "Nothing to run"
+        if pack.length == 1:
+            result = self.call("shell am start " + pack[0])    
+        elif pack.length == 2:
+            result = self.call("shell am start " + pack[0] + "/." + pack[1])
+        elif pack.length == 3:
+            result = self.call("shell am start " + pack[0] + " " + pack[1] + "/." + pack[2])
+        return result
+
+    def screen(self, res):
+        result = self.call("display-size " + res)
+        return result
+
+    def dpi(self, dpi):
+        result = self.call("display-density " + dpi)
+        return result
+
+    def screenRecord(self, param):
+        params = param.split()
+        if params.length == 1:
+            result = self.call("shell screenrecord " + params[0])
+        elif params.length == 2:
+            result = self.call("shell screenrecord --time-limit " + params[0] + " " + params[1])
+        return result
